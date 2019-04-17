82e0c816d64cfbf4c8d692d366a2778fc7650591
nabtron <developer@nabtron.com>
2019-04-18 01:05:41 +0500

    Create _custom_swiftype.php

---
+++ _custom_swiftype.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * custom swiftype.
+ *
+ * @wordpress-plugin
+ * Plugin Name: custom swiftype
+ * Version:     0.0.4
+ * Plugin URI:  https://nabtron.com
+ * Description: custom swiftype.
+ * Author:      nabtron
+ * Author URI:  https://nabtron
+ *
+ **/
+
+include_once('inc/class-nabtron-call-custom-swiftype.php');
+include_once('inc/functions.php');
